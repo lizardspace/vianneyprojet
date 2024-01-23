bb96368e05908c9a2b792e952ce5514891c2b016
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-01-23 17:10:52 +0000

    fgfdgdfhgfdgg

--- src/components/navbar/NavbarLinksAdmin.js
+++ src/components/navbar/NavbarLinksAdmin.js
@@ -1,14 +1,20 @@
-// Chakra Imports
 import {
 	Flex,
+	Image,
+	Menu,
+	MenuList,
 	useColorModeValue
 } from '@chakra-ui/react';
-// Custom Components
 import DropdownMenu from 'components/navbar/searchBar/DropdownMenu';
+import { SidebarResponsive } from 'components/sidebar/Sidebar';
 import PropTypes from 'prop-types';
 import React from 'react';
+// Assets
+import navImage from 'assets/img/layout/Navbar.png';
+import routes from 'routes.js';
 export default function HeaderLinks(props) {
 	const { secondary } = props;
+	// Chakra Color Mode
 	let menuBg = useColorModeValue('white', 'navy.800');
 	const shadow = useColorModeValue(
 		'14px 17px 40px 4px rgba(112, 144, 176, 0.18)',
@@ -24,7 +30,26 @@ export default function HeaderLinks(props) {
 			p="10px"
 			borderRadius="30px"
 			boxShadow={shadow}>
-			<DropdownMenu mb={secondary ? { base: '10px', md: 'unset' } : 'unset'} me="10px" borderRadius="30px" />		
+			<DropdownMenu mb={secondary ? { base: '10px', md: 'unset' } : 'unset'} me="10px" borderRadius="30px" />
+			<SidebarResponsive routes={routes} />
+			<Menu>
+				<MenuList
+					boxShadow={shadow}
+					p='20px'
+					me={{ base: "30px", md: "unset" }}
+					borderRadius='20px'
+					bg={menuBg}
+					border='none'
+					mt='22px'
+					minW={{ base: "unset" }}
+					maxW={{ base: "360px", md: "unset" }}>
+					<Image src={navImage} borderRadius='16px' mb='28px' />
+				</MenuList>
+			</Menu>
+
+
+
+
 		</Flex>
 	);
 }
