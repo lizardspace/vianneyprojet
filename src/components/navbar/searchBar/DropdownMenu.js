7b08d7352d13422eee7b15d429956f3bd52d57c8
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-01-23 12:23:15 +0000

    rgregggtrhrthrt

--- src/components/navbar/searchBar/DropdownMenu.js
+++ src/components/navbar/searchBar/DropdownMenu.js
@@ -9,6 +9,7 @@ import {
   MenuItem,
 } from '@chakra-ui/react';
 import { createClient } from '@supabase/supabase-js';
+import { FcExpand, FcCollapse } from 'react-icons/fc';
 
 const supabaseUrl = 'https://hvjzemvfstwwhhahecwu.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2anplbXZmc3R3d2hoYWhlY3d1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MTQ4Mjc3MCwiZXhwIjoyMDA3MDU4NzcwfQ.6jThCX2eaUjl2qt4WE3ykPbrh6skE8drYcmk-UCNDSw';
@@ -17,6 +18,7 @@ const supabase = createClient(supabaseUrl, supabaseAnonKey);
 const DropdownMenu = () => {
   const [selectedItem, setSelectedItem] = useState(null);
   const [eventList, setEventList] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     // Fetch data from the vianney_event table
@@ -39,10 +41,14 @@ const DropdownMenu = () => {
     setSelectedItem(item);
   };
 
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <Box>
       <Menu>
-        <MenuButton as={Button} rightIcon="chevron-down">
+        <MenuButton as={Button} rightIcon={isOpen ? <FcCollapse /> : <FcExpand />}>
           Choisissez l'évênement
         </MenuButton>
         <MenuList>
