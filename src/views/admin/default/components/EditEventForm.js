cd2f8d06a0469e2a8218b9277ecc040fa9767235
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-01-25 17:36:15 +0000

    gnbbnhnhhjhj

--- src/views/admin/default/components/EditEventForm.js
+++ src/views/admin/default/components/EditEventForm.js
@@ -16,8 +16,8 @@ import {
 } from '@chakra-ui/react';
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'YOUR_SUPABASE_URL';
-const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+const supabaseUrl = 'https://hvjzemvfstwwhhahecwu.supabase.co';
+const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2anplbXZmc3R3d2hoYWhlY3d1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MTQ4Mjc3MCwiZXhwIjoyMDA3MDU4NzcwfQ.6jThCX2eaUjl2qt4WE3ykPbrh6skE8drYcmk-UCNDSw';
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 export default function EditEventForm({ event, refreshEvents }) {
