4a377e64910e70de4b961faa0eb3f82506b90b93
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-01-26 18:29:06 +0000

    fnhghgjhjh

--- src/views/admin/carte/components/UserForm.js
+++ src/views/admin/carte/components/UserForm.js
@@ -13,14 +13,15 @@ import {
   VStack,
   HStack,
   Checkbox,
+  Alert, AlertIcon, AlertTitle, AlertDescription, CloseButton,
 } from '@chakra-ui/react';
-import { useEvent } from '../../../../EventContext'; 
+import { useEvent } from '../../../../EventContext';
 const supabaseUrl = 'https://hvjzemvfstwwhhahecwu.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imh2anplbXZmc3R3d2hoYWhlY3d1Iiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTY5MTQ4Mjc3MCwiZXhwIjoyMDA3MDU4NzcwfQ.6jThCX2eaUjl2qt4WE3ykPbrh6skE8drYcmk-UCNDSw';
 const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
 const UserForm = () => {
-  const { selectedEventId } = useEvent(); 
+  const { selectedEventId } = useEvent();
   const [nameOfTheTeam, setNameOfTheTeam] = useState('');
   const [profilePhoto, setProfilePhoto] = useState(null);
   const [lat, setLat] = useState(45.75799485263588);
@@ -29,16 +30,17 @@ const UserForm = () => {
   const [typeDeVehicule, setTypeDeVehicule] = useState('');
   const [immatriculation, setImmatriculation] = useState('');
   const [specialite, setSpecialite] = useState('');
-  const [teamMembers, setTeamMembers] = useState([{ 
+  const [showSuccessAlert, setShowSuccessAlert] = useState(false);
+  const [teamMembers, setTeamMembers] = useState([{
     id: uuidv4(), // Generate unique ID for the first team member
-    familyname: '', 
-    firstname: '', 
-    mail: '', 
-    phone: '', 
+    familyname: '',
+    firstname: '',
+    mail: '',
+    phone: '',
     isLeader: false, // Added isLeader property
   }]);
   const generateRandomColor = () => {
-    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    const randomColor = Math.floor(Math.random() * 16777215).toString(16);
     return `#${randomColor}`;
   };
   const handleFileChange = (e) => {
@@ -47,7 +49,7 @@ const UserForm = () => {
 
   const handleTeamMemberChange = (index, event) => {
     let values = [...teamMembers];
-  
+
     if (event.target.name === 'isLeader') {
       // Set all isLeader properties to false
       values = values.map(member => ({ ...member, isLeader: false }));
@@ -56,35 +58,35 @@ const UserForm = () => {
     } else {
       values[index][event.target.name] = event.target.value;
     }
-  
+
     setTeamMembers(values);
   };
-  
+
 
   const handleAddTeamMember = () => {
-    setTeamMembers([...teamMembers, { 
+    setTeamMembers([...teamMembers, {
       id: uuidv4(), // Generate unique ID for new team member
-      familyname: '', 
-      firstname: '', 
-      mail: '', 
-      phone: '' 
+      familyname: '',
+      firstname: '',
+      mail: '',
+      phone: ''
     }]);
   };
 
   // Map events
-const LocationMarker = () => {
-  const map = useMapEvents({
-    click(e) {
-      setLat(e.latlng.lat);
-      setLng(e.latlng.lng);
-      map.flyTo(e.latlng, map.getZoom());
-    },
-  });
+  const LocationMarker = () => {
+    const map = useMapEvents({
+      click(e) {
+        setLat(e.latlng.lat);
+        setLng(e.latlng.lng);
+        map.flyTo(e.latlng, map.getZoom());
+      },
+    });
 
-  return lat !== 0 ? (
-    <Marker position={[lat, lng]}></Marker>
-  ) : null;
-};
+    return lat !== 0 ? (
+      <Marker position={[lat, lng]}></Marker>
+    ) : null;
+  };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -162,88 +164,97 @@ const LocationMarker = () => {
           <Input id='profile-photo' type="file" onChange={handleFileChange} />
         </FormControl>
         <FormControl>
-        <FormLabel htmlFor='mission'>Mission</FormLabel>
-        <Input
-          id='mission'
-          type="text"
-          value={mission}
-          onChange={(e) => setMission(e.target.value)}
-        />
-      </FormControl>
-      <FormControl>
-        <FormLabel htmlFor='typeDeVehicule'>Type de Véhicule</FormLabel>
-        <Input
-          id='typeDeVehicule'
-          type="text"
-          value={typeDeVehicule}
-          onChange={(e) => setTypeDeVehicule(e.target.value)}
-        />
-      </FormControl>
+          <FormLabel htmlFor='mission'>Mission</FormLabel>
+          <Input
+            id='mission'
+            type="text"
+            value={mission}
+            onChange={(e) => setMission(e.target.value)}
+          />
+        </FormControl>
+        <FormControl>
+          <FormLabel htmlFor='typeDeVehicule'>Type de Véhicule</FormLabel>
+          <Input
+            id='typeDeVehicule'
+            type="text"
+            value={typeDeVehicule}
+            onChange={(e) => setTypeDeVehicule(e.target.value)}
+          />
+        </FormControl>
 
-      <FormControl>
-        <FormLabel htmlFor='immatriculation'>Immatriculation</FormLabel>
-        <Input
-          id='immatriculation'
-          type="text"
-          value={immatriculation}
-          onChange={(e) => setImmatriculation(e.target.value)}
-        />
-      </FormControl>
+        <FormControl>
+          <FormLabel htmlFor='immatriculation'>Immatriculation</FormLabel>
+          <Input
+            id='immatriculation'
+            type="text"
+            value={immatriculation}
+            onChange={(e) => setImmatriculation(e.target.value)}
+          />
+        </FormControl>
 
-      <FormControl>
-        <FormLabel htmlFor='specialite'>Spécialité</FormLabel>
-        <Input
-          id='specialite'
-          type="text"
-          value={specialite}
-          onChange={(e) => setSpecialite(e.target.value)}
-        />
-      </FormControl>
+        <FormControl>
+          <FormLabel htmlFor='specialite'>Spécialité</FormLabel>
+          <Input
+            id='specialite'
+            type="text"
+            value={specialite}
+            onChange={(e) => setSpecialite(e.target.value)}
+          />
+        </FormControl>
 
-    {teamMembers.map((teamMember, index) => (
-      <HStack key={index} spacing={2}>
-        <Input
-      type="text"
-      name="familyname" // Add this line
-      placeholder="Nom de famille"
-      value={teamMember.familyname}
-      onChange={(e) => handleTeamMemberChange(index, e)}
-    />
-    <Input
-      type="text"
-      name="firstname" // Add this line
-      placeholder="Prénom"
-      value={teamMember.firstname}
-      onChange={(e) => handleTeamMemberChange(index, e)}
-    />
-    <Input
-      type="text"
-      name="mail" // Add this line
-      placeholder="Email"
-      value={teamMember.mail}
-      onChange={(e) => handleTeamMemberChange(index, e)}
-    />
-    <Input
-      type="text"
-      name="phone" // Add this line
-      placeholder="Téléphone"
-      value={teamMember.phone}
-      onChange={(e) => handleTeamMemberChange(index, e)}
-    />
-        <Checkbox 
-          name="isLeader"
-          isChecked={teamMember.isLeader}
-          onChange={(e) => handleTeamMemberChange(index, e)}
-        >
-          Leader ?
-        </Checkbox>
-      </HStack>
-    ))}
-    
-
+        {teamMembers.map((teamMember, index) => (
+          <HStack key={index} spacing={2}>
+            <Input
+              type="text"
+              name="familyname" // Add this line
+              placeholder="Nom de famille"
+              value={teamMember.familyname}
+              onChange={(e) => handleTeamMemberChange(index, e)}
+            />
+            <Input
+              type="text"
+              name="firstname" // Add this line
+              placeholder="Prénom"
+              value={teamMember.firstname}
+              onChange={(e) => handleTeamMemberChange(index, e)}
+            />
+            <Input
+              type="text"
+              name="mail" // Add this line
+              placeholder="Email"
+              value={teamMember.mail}
+              onChange={(e) => handleTeamMemberChange(index, e)}
+            />
+            <Input
+              type="text"
+              name="phone" // Add this line
+              placeholder="Téléphone"
+              value={teamMember.phone}
+              onChange={(e) => handleTeamMemberChange(index, e)}
+            />
+            <Checkbox
+              name="isLeader"
+              isChecked={teamMember.isLeader}
+              onChange={(e) => handleTeamMemberChange(index, e)}
+            >
+              Leader ?
+            </Checkbox>
+          </HStack>
+        ))}
+        {showSuccessAlert && (
+          <Alert status="success" variant="subtle" flexDirection="column" alignItems="center" justifyContent="center" textAlign="center" mt={4}>
+            <AlertIcon boxSize="40px" mr={0} />
+            <AlertTitle mt={4} mb={1} fontSize="lg">
+              User data added successfully
+            </AlertTitle>
+            <AlertDescription maxWidth="sm">
+              Your user data has been added successfully.
+            </AlertDescription>
+            <CloseButton position="absolute" right="8px" top="8px" onClick={() => setShowSuccessAlert(false)} />
+          </Alert>
+        )}
         <Button colorScheme="blue" onClick={handleAddTeamMember}>Ajouter un membre de l'équipe</Button>
       </VStack>
-
       <Button mt={4} colorScheme="green" type="submit">Ajouter l'utilisateur</Button>
     </form>
   );
