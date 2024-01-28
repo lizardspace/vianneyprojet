22a7ba1708922b6251e87bc5485331fb83ddbaa5
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-01-28 17:48:51 +0000

    ddfsgdffdff

--- src/views/admin/Parameters/modal/EventDateComponent.js
+++ src/views/admin/Parameters/modal/EventDateComponent.js
@@ -1,34 +1,46 @@
-import React, { useEffect, useState} from 'react';
-import { Box, Text } from '@chakra-ui/react';
+import React, { useEffect, useState } from 'react';
+import { Alert, AlertIcon, Box, Text, Spinner } from '@chakra-ui/react';
 import { supabase } from '../../../../supabaseClient';
 import { useEvent } from './../../../../EventContext';
+import { FcCalendar } from "react-icons/fc";
 
 const EventDateComponent = () => {
   const { selectedEventId } = useEvent();
   const [eventDate, setEventDate] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     if (selectedEventId) {
+      setIsLoading(true);
+      setError(null);
+
       // Fetch the specific event data from the Supabase table using the selected event ID
       async function fetchEventDate() {
-        const { data, error } = await supabase
-          .from('vianney_event')
-          .select('event_date')
-          .eq('event_id', selectedEventId)
-          .single();
+        try {
+          const { data, error } = await supabase
+            .from('vianney_event')
+            .select('event_date')
+            .eq('event_id', selectedEventId)
+            .single();
 
-        if (error) {
-          console.error('Error fetching event date:', error.message);
-        } else {
-          // Format the date in French format (you can use a library like date-fns for more advanced formatting)
-          const formattedDate = new Date(data.event_date).toLocaleDateString('fr-FR', {
-            weekday: 'long',
-            year: 'numeric',
-            month: 'long',
-            day: 'numeric',
-          });
+          if (error) {
+            setError('Error fetching event date');
+          } else {
+            // Format the date in French format (you can use a library like date-fns for more advanced formatting)
+            const formattedDate = new Date(data.event_date).toLocaleDateString('fr-FR', {
+              weekday: 'long',
+              year: 'numeric',
+              month: 'long',
+              day: 'numeric',
+            });
 
-          setEventDate(formattedDate);
+            setEventDate(formattedDate);
+          }
+        } catch (error) {
+          setError('Error fetching event date');
+        } finally {
+          setIsLoading(false);
         }
       }
 
@@ -39,12 +51,19 @@ const EventDateComponent = () => {
   }, [selectedEventId]);
 
   return (
-    <Box>
+    <Alert status="info" variant="subtle" flexDirection="column" alignItems="center">
+      <AlertIcon as={FcCalendar} boxSize={8} />
       <Text fontSize='xl' m={4}>
         Date de l'événement :
       </Text>
-      <Text fontSize='lg'>{eventDate}</Text>
-    </Box>
+      {isLoading ? (
+        <Spinner size="lg" />
+      ) : error ? (
+        <Text color="red.500">{error}</Text>
+      ) : (
+        <Text fontSize='lg'>{eventDate}</Text>
+      )}
+    </Alert>
   );
 };
 
