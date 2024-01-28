50021d8edbc6cc1b3ff3fd6ad63474de15b77436
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-01-28 14:07:44 +0000

    dssddsddfddfff

--- src/views/admin/carte/components/TableTopCreators.js
+++ src/views/admin/carte/components/TableTopCreators.js
@@ -21,32 +21,23 @@ function TopCreatorTable(props) {
         direction='column'
         w='100%'
         overflowX={{ sm: "scroll", lg: "hidden" }}>
-        <Flex
-          align={{ sm: "flex-start", lg: "center" }}
-          justify='space-between'
-          w='100%'
-          px='22px'
-          pb='20px'
-          mb='10px'
-          boxShadow='0px 40px 58px -20px rgba(112, 144, 176, 0.26)'>
-          <Heading me='auto'
-            color={textColor}
-            fontSize='2xl'
-            fontWeight='700'
-            lineHeight='100%'
-            mb="20px">
-            Les équipiers en intervention
-          </Heading>
-          <Button variant='action' onClick={handleToggleShowAll}>
-            {showAll ? 'Voir moins' : 'Les voir tous'}
-          </Button>
-        </Flex>
-        <Flex
-          direction='column'
-          overflowY={showAll ? 'scroll' : 'hidden'}
-          maxH={showAll ? '300px' : 'auto'}>
-          <EquipiersTable showAll={showAll} />
-        </Flex>
+        <Heading me='auto'
+          color={textColor}
+          fontSize='2xl'
+          fontWeight='700'
+          lineHeight='100%'
+          mb="20px">
+          Les équipiers en intervention
+        </Heading>
+      </Flex>
+      <Flex
+        direction='column'
+        overflowY={showAll ? 'scroll' : 'hidden'}
+        maxH={showAll ? '300px' : 'auto'}>
+        <EquipiersTable showAll={showAll} />
+        <Button variant='action' onClick={handleToggleShowAll}>
+          {showAll ? 'Voir moins' : 'Voir tout le monde'}
+        </Button>
       </Flex>
     </>
   );
