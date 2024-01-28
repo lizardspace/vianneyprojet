8ed2cf05357bb55b0ca062e957a166ae9527c188
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-01-28 17:43:37 +0000

    jhhjkkfff

--- src/views/admin/Parameters/modal/EventDateComponent.js
+++ src/views/admin/Parameters/modal/EventDateComponent.js
@@ -1,35 +1,42 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState} from 'react';
 import { Box, Text } from '@chakra-ui/react';
-import { supabase } from '../../../../supabaseClient'; // Import your Supabase configuration here
+import { supabase } from '../../../../supabaseClient';
+import { useEvent } from './../../../../EventContext';
 
 const EventDateComponent = () => {
+  const { selectedEventId } = useEvent();
   const [eventDate, setEventDate] = useState(null);
 
   useEffect(() => {
-    // Fetch the event data from the Supabase table
-    async function fetchEventDate() {
-      const { data, error } = await supabase
-        .from('vianney_event')
-        .select('event_date')
-        .single();
+    if (selectedEventId) {
+      // Fetch the specific event data from the Supabase table using the selected event ID
+      async function fetchEventDate() {
+        const { data, error } = await supabase
+          .from('vianney_event')
+          .select('event_date')
+          .eq('event_id', selectedEventId)
+          .single();
 
-      if (error) {
-        console.error('Error fetching event date:', error.message);
-      } else {
-        // Format the date in French format (you can use a library like date-fns for more advanced formatting)
-        const formattedDate = new Date(data.event_date).toLocaleDateString('fr-FR', {
-          weekday: 'long',
-          year: 'numeric',
-          month: 'long',
-          day: 'numeric',
-        });
+        if (error) {
+          console.error('Error fetching event date:', error.message);
+        } else {
+          // Format the date in French format (you can use a library like date-fns for more advanced formatting)
+          const formattedDate = new Date(data.event_date).toLocaleDateString('fr-FR', {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+          });
 
-        setEventDate(formattedDate);
+          setEventDate(formattedDate);
+        }
       }
-    }
 
-    fetchEventDate();
-  }, []);
+      fetchEventDate();
+    } else {
+      setEventDate(null); // Clear the date if no event is selected
+    }
+  }, [selectedEventId]);
 
   return (
     <Box>
