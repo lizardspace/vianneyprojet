477ef2a951e155fab62f80e3a0a62c0116af1a12
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-01-28 17:29:49 +0000

    gggghggggg

--- src/views/admin/Parameters/components/ParametersEvents.js
+++ src/views/admin/Parameters/components/ParametersEvents.js
@@ -1,11 +1,17 @@
-import React from 'react';
-import { Box, Text, Button, useColorModeValue } from '@chakra-ui/react';
+import React, {useState} from 'react';
+import { Box, Text, Button, useColorModeValue, ModalOverlay, Modal, ModalCloseButton, ModalHeader, ModalBody, ModalContent,} from '@chakra-ui/react';
 import { FcDocument } from "react-icons/fc";
 
 const ParametersEvents = () => {
   const brandColor = useColorModeValue("brand.500", "white");
   const buttonBg = useColorModeValue("white", "gray.800");
   const buttonTextColor = useColorModeValue("secondaryGray.900", "white");
+
+  // State for managing the modal
+  const [showDocumentsModal, setShowDocumentsModal] = useState(false);
+
+  const openDocumentsModal = () => setShowDocumentsModal(true);
+  const closeDocumentsModal = () => setShowDocumentsModal(false);
 
   return (
     <Box border='1px' borderColor='gray.200' p={5} m={5}>
@@ -15,12 +21,23 @@ const ParametersEvents = () => {
         bg={buttonBg}
         color={buttonTextColor}
         h='100px'
-        m={4} // Margin bottom for spacing
+        m={4}
+        onClick={openDocumentsModal}
       >
         Documents missions
       </Button>
 
-      {/* Add more content here */}
+      {/* Modal */}
+      <Modal isOpen={showDocumentsModal} onClose={closeDocumentsModal}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Documents missions Modal</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {/* Add your content for the Documents missions modal here */}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };
