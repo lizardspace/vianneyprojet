99941c6df85e9c0e4256cfb3b0a01f780886995c
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-01-28 17:26:55 +0000

    hhhhhhhhh

--- src/views/admin/Parameters/components/ParametersEventAndCharacteristics.js
+++ src/views/admin/Parameters/components/ParametersEventAndCharacteristics.js
@@ -1,11 +1,33 @@
-import React from 'react';
-import { Box, Text, Button, useColorModeValue } from '@chakra-ui/react';
+import React, {useState} from 'react';
+import { Box, Text, Button, useColorModeValue, ModalCloseButton, ModalHeader, ModalBody, ModalOverlay, Modal, ModalContent,} from '@chakra-ui/react';
 import { FcBusiness, FcCalendar, FcHome, FcList, FcVoicePresentation } from "react-icons/fc";
 
 const ParametersEventAndCharacteristics = () => {
   const brandColor = useColorModeValue("brand.500", "white");
   const buttonBg = useColorModeValue("white", "gray.800");
   const buttonTextColor = useColorModeValue("secondaryGray.900", "white");
+
+  // State for managing modals
+  const [showEventNameModal, setShowEventNameModal] = useState(false);
+  const [showEventDateModal, setShowEventDateModal] = useState(false);
+  const [showEventLocationModal, setShowEventLocationModal] = useState(false);
+  const [showEventNeedsModal, setShowEventNeedsModal] = useState(false);
+  const [showEventMailingModal, setShowEventMailingModal] = useState(false);
+
+  const openEventNameModal = () => setShowEventNameModal(true);
+  const closeEventNameModal = () => setShowEventNameModal(false);
+
+  const openEventDateModal = () => setShowEventDateModal(true);
+  const closeEventDateModal = () => setShowEventDateModal(false);
+
+  const openEventLocationModal = () => setShowEventLocationModal(true);
+  const closeEventLocationModal = () => setShowEventLocationModal(false);
+
+  const openEventNeedsModal = () => setShowEventNeedsModal(true);
+  const closeEventNeedsModal = () => setShowEventNeedsModal(false);
+
+  const openEventMailingModal = () => setShowEventMailingModal(true);
+  const closeEventMailingModal = () => setShowEventMailingModal(false);
 
   return (
     <Box border='1px' borderColor='gray.200' p={5} m={5}>
@@ -16,7 +38,8 @@ const ParametersEventAndCharacteristics = () => {
         bg={buttonBg}
         color={buttonTextColor}
         h='100px'
-        m={4} // Margin for spacing
+        m={4}
+        onClick={openEventNameModal}
       >
         Nom de l'événement
       </Button>
@@ -26,7 +49,8 @@ const ParametersEventAndCharacteristics = () => {
         bg={buttonBg}
         color={buttonTextColor}
         h='100px'
-        m={4} // Margin for spacing
+        m={4}
+        onClick={openEventDateModal}
       >
         Date de l'événement
       </Button>
@@ -36,7 +60,8 @@ const ParametersEventAndCharacteristics = () => {
         bg={buttonBg}
         color={buttonTextColor}
         h='100px'
-        m={4} // Margin for spacing
+        m={4}
+        onClick={openEventLocationModal}
       >
         Lieu de l'événement
       </Button>
@@ -46,7 +71,8 @@ const ParametersEventAndCharacteristics = () => {
         bg={buttonBg}
         color={buttonTextColor}
         h='100px'
-        m={4} // Margin for spacing
+        m={4}
+        onClick={openEventNeedsModal}
       >
         Besoins
       </Button>
@@ -56,12 +82,67 @@ const ParametersEventAndCharacteristics = () => {
         bg={buttonBg}
         color={buttonTextColor}
         h='100px'
-        m={4} // Margin for spacing
+        m={4}
+        onClick={openEventMailingModal}
       >
         Mailing
       </Button>
 
-      {/* Add more content here */}
+      {/* Modals */}
+      <Modal isOpen={showEventNameModal} onClose={closeEventNameModal}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Nom de l'événement Modal</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {/* Add your content for the Nom de l'événement modal here */}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+
+      <Modal isOpen={showEventDateModal} onClose={closeEventDateModal}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Date de l'événement Modal</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {/* Add your content for the Date de l'événement modal here */}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+
+      <Modal isOpen={showEventLocationModal} onClose={closeEventLocationModal}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Lieu de l'événement Modal</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {/* Add your content for the Lieu de l'événement modal here */}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+
+      <Modal isOpen={showEventNeedsModal} onClose={closeEventNeedsModal}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Besoins Modal</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {/* Add your content for the Besoins modal here */}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+
+      <Modal isOpen={showEventMailingModal} onClose={closeEventMailingModal}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Mailing Modal</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            {/* Add your content for the Mailing modal here */}
+          </ModalBody>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 };
