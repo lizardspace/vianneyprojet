4175fe682e5ffe245184512a038b97e6d978d597
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-01-30 20:39:42 +0000

    fggffgfgfgfgg

--- src/views/admin/Parameters/modal/SalleDeCrise.js
+++ src/views/admin/Parameters/modal/SalleDeCrise.js
@@ -1,17 +1,28 @@
-import ContactInfoForm from './salledecrise/ContactInfoForm';
-import Documents from './salledecrise/Documents';
-import FormDataViewer from './salledecrise/FormDataViewer';
-import NotepadComponentSalleDeCrise from './salledecrise/NotepadComponentSalleDeCrise';
+import React, { useState } from 'react';
 import {
   Heading,
   useColorModeValue,
+  Button,
 } from '@chakra-ui/react';
+import ContactInfoForm from './salledecrise/ContactInfoForm';
+import FormDataViewer from './salledecrise/FormDataViewer';
+import NotepadComponentSalleDeCrise from './salledecrise/NotepadComponentSalleDeCrise';
+import Documents from './salledecrise/Documents';
 
 const SalleDeCrise = () => {
   const textColor = useColorModeValue("secondaryGray.900", "white");
+  const [showContactInfoForm, setShowContactInfoForm] = useState(false); // Initially set to true
+
+  const toggleContactInfoForm = () => {
+    setShowContactInfoForm(!showContactInfoForm); // Toggle the state
+  };
+
   return (
     <div>
-      <ContactInfoForm />
+      <Button onClick={toggleContactInfoForm}>
+        {showContactInfoForm ? "Hide Contact Info Form" : "Show Contact Info Form"}
+      </Button>
+      {showContactInfoForm && <ContactInfoForm />}      
       <FormDataViewer />
       <Heading me='auto' color={textColor} fontSize='2xl' fontWeight='700' lineHeight='100%' mb="20px">Bloc Note</Heading>
       <NotepadComponentSalleDeCrise />
