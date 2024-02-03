99cbc2001ff244faad8786c2ac71c9a3e0c348ee
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-02-03 16:46:54 +0000

    hjhgjhghjhjhjj

--- src/views/admin/InterfaceEquipe/index.js
+++ src/views/admin/InterfaceEquipe/index.js
@@ -1,15 +1,36 @@
-// src/views/admin/Parameters/index.jsx
 import React from 'react';
-import { Box } from '@chakra-ui/react';
+import { Box, Heading, useColorModeValue } from '@chakra-ui/react';
 import GpsPosition from './components/GpsPosition';
 import Audio from './components/Audio';
 
 const InterfaceEquipe = () => {
+  const textColor = useColorModeValue("secondaryGray.900", "white"); // Define textColor based on the color mode
 
   return (
     <Box pt={{ base: "180px", md: "80px", xl: "80px" }}>
-      <Audio/>
-      <GpsPosition/>
+      <Heading
+        me="auto"
+        color={textColor}
+        fontSize="2xl"
+        fontWeight="700"
+        lineHeight="100%"
+        mb="20px"
+      >
+        Radio CB
+      </Heading>
+      <Audio />
+      <Heading
+        me="auto"
+        color={textColor}
+        fontSize="2xl"
+        fontWeight="700"
+        lineHeight="100%"
+        mb={10}
+        mt={10}
+      >
+        La position que vous communiquez au PC sécurité
+      </Heading>
+      <GpsPosition />
     </Box>
   );
 };
