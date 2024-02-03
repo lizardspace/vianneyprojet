c6d562a0d84c0f8be6c24a81d4bd6cedfe854bdb
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-02-03 16:34:16 +0000

    fbgfbfbhghghggg

--- src/views/admin/InterfaceEquipe/index.js
+++ src/views/admin/InterfaceEquipe/index.js
@@ -1,7 +1,6 @@
 // src/views/admin/Parameters/index.jsx
 import React from 'react';
 import { Box } from '@chakra-ui/react';
-import TogglingSpeaker from './components/TogglingSpeaker';
 import GpsPosition from './components/GpsPosition';
 import Audio from './components/Audio';
 
@@ -9,7 +8,6 @@ const InterfaceEquipe = () => {
 
   return (
     <Box pt={{ base: "180px", md: "80px", xl: "80px" }}>
-      <TogglingSpeaker/>
       <Audio/>
       <GpsPosition/>
     </Box>
