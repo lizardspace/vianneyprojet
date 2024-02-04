e80d91340abc0d42308779e0f2db2558f2baf10a
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-02-04 04:52:01 +0000

    fgfdjfyjfyjyyy

--- src/views/admin/InterfaceEquipe/components/GpsPosition.js
+++ src/views/admin/InterfaceEquipe/components/GpsPosition.js
@@ -50,31 +50,43 @@ const GpsPosition = () => {
     const showPosition = (newPosition) => {
       const { latitude, longitude } = newPosition.coords;
 
-      // Initialize the map if it hasn't been initialized yet
-      if (!mapInitialized) {
+      // Check if mapRef.current exists and is not destroyed
+      if (mapRef.current && !mapRef.current._leaflet_id) {
+        // Re-create the map if it's destroyed
         mapRef.current = L.map('map', {
           zoomControl: false, // Disable the default zoom control
-        }).setView([latitude, longitude], 13);
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-          maxZoom: 19,
-          attribution: '',
-        }).addTo(mapRef.current);
-
-        // Add a custom zoom control
-        L.control.zoom({ position: 'bottomright' }).addTo(mapRef.current);
-
-        // Set mapInitialized to true to prevent re-initialization
-        setMapInitialized(true);
+        });
+        setMapInitialized(false);
+      } else if (!mapRef.current) {
+        // Create the map if it hasn't been initialized yet
+        mapRef.current = L.map('map', {
+          zoomControl: false, // Disable the default zoom control
+        });
+        setMapInitialized(false);
       }
 
-      // Create a custom icon using the MdPlace icon
-      const customIcon = createCustomIcon();
-
-      // Update the marker position with the custom icon
+      // Set the view if the map exists
       if (mapRef.current) {
+        if (!mapInitialized) {
+          // Initialize the map if it hasn't been initialized yet
+          L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '',
+          }).addTo(mapRef.current);
+
+          // Add a custom zoom control
+          L.control.zoom({ position: 'bottomright' }).addTo(mapRef.current);
+          setMapInitialized(true);
+        }
+
+        // Create a custom icon using the MdPlace icon
+        const customIcon = createCustomIcon();
+
+        // Update the marker position with the custom icon
         L.marker([latitude, longitude], {
           icon: customIcon,
         }).addTo(mapRef.current);
+
         mapRef.current.setView([latitude, longitude], 13);
       }
     };
@@ -132,7 +144,7 @@ const GpsPosition = () => {
         navigator.geolocation.clearWatch(watchId);
       }
     };
-  }, [toast, watchId, mapInitialized]); // Include mapInitialized as a dependency in the useEffect
+  }, [toast, watchId, mapInitialized]);
 
   return (
     <Box p={4}>
