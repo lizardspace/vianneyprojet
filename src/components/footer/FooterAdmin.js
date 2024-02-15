5e785c82b33775b8533688aff0ea96edc0d508b6
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-02-15 14:41:20 +0000

    hhhhhhhhhhhhhhhhhhhh

--- src/components/footer/FooterAdmin.js
+++ src/components/footer/FooterAdmin.js
@@ -13,7 +13,6 @@ import {
 
 export default function Footer() {
   const textColor = useColorModeValue("gray.400", "white");
-  const { toggleColorMode } = useColorMode();
   return (
     <Flex
       zIndex='3'
