ee1c4fdc976335f2cba8d0e8aee6988b4d083f7f
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-02-17 22:26:40 +0000

    rgegtrtgrert

--- src/views/admin/AlerteEquipe/components/UrgentAlerts.js
+++ src/views/admin/AlerteEquipe/components/UrgentAlerts.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { format } from 'date-fns';
 import {
   Alert,
   AlertIcon,
@@ -34,48 +35,48 @@ const UrgentAlerts = () => {
     async function fetchUrgentAlerts() {
       try {
         let query = supabase.from('vianney_alertes_specifiques').select("*").order('created_at', { ascending: false }).limit(5);
-        
+
         // Filter alerts based on the selected event ID
         if (selectedEventId) {
           query = query.eq('event_id', selectedEventId);
         }
-  
+
         const { data: alertsData, error: alertsError } = await query;
-  
+
         if (alertsError) {
           throw alertsError;
         }
-  
+
         // Fetch team names corresponding to teams_id
         const teamIds = alertsData.map(alert => alert.teams_id);
         const { data: teamsData, error: teamsError } = await supabase.from('vianney_teams').select('id, name_of_the_team').in('id', teamIds);
-  
+
         if (teamsError) {
           throw teamsError;
         }
-  
+
         // Create a map of team id to team name
         const teamNameMap = {};
         teamsData.forEach(team => {
           teamNameMap[team.id] = team.name_of_the_team;
         });
-  
+
         // Combine alert data with team names
         const enrichedAlertsData = alertsData.map(alert => ({
           ...alert,
           team_name: teamNameMap[alert.teams_id]
         }));
-  
+
         setUrgentAlerts(enrichedAlertsData);
         setIsLoading(false);
       } catch (error) {
         console.error("Error fetching urgent alerts:", error.message);
       }
     }
-  
+
     fetchUrgentAlerts();
   }, [selectedEventId]); // Re-fetch alerts when selectedEventId changes
-  
+
 
   const handleToggleReadStatus = async (id, currentStatus) => {
     try {
@@ -150,14 +151,13 @@ const UrgentAlerts = () => {
               pr={14}
               rounded="md"
             >
-              <AlertIcon />
-              <Stack spacing={2}>
+              <Box display="flex" alignItems="center"> {/* Utilisez une boîte flex pour aligner les éléments sur la même ligne */}
+                <AlertIcon />
                 <Text fontWeight="bold" fontSize="lg">
                   {alert.text_alert}
                 </Text>
-                <Text fontSize="sm" color="gray.500">
-                  Équipe: {alert.team_name} {alert.teams_id}
-                </Text>
+              </Box>
+              <Stack spacing={2}>
                 <Button
                   onClick={() =>
                     handleToggleReadStatus(alert.id, alert.read_or_not)
@@ -168,14 +168,15 @@ const UrgentAlerts = () => {
                 >
                   {alert.read_or_not ? "Lu" : "Non lu"}
                 </Button>
-                <Text fontSize="sm" color="gray.500">
-                  Créé le: {new Date(alert.created_at).toLocaleString()}
-                </Text>
                 {alert.response && (
                   <Text fontWeight="bold" fontSize="lg">
-                    Réponse: {alert.response}
+                    Réponse de l'équipe {alert.team_name} : {alert.response}
                   </Text>
                 )}
+                <Text fontSize="sm" color="gray.500">
+                  Créé le  {format(new Date(alert.created_at), "dd/MM/yyyy à HH:mm")} pour {alert.team_name}
+                </Text>
+
               </Stack>
             </Alert>
           ))}
@@ -209,5 +210,5 @@ const UrgentAlerts = () => {
     </Box>
   );
 };
-  
+
 export default UrgentAlerts;
--- src/views/admin/InterfaceEquipe/components/UrgentAlerts.js
+++ src/views/admin/InterfaceEquipe/components/UrgentAlerts.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { format } from 'date-fns';
 import {
   Alert,
   AlertIcon,
@@ -18,10 +19,10 @@ import {
   FormControl,
   FormLabel,
   FormHelperText,
-  } from "@chakra-ui/react";
+} from "@chakra-ui/react";
 import { supabase } from './../../../../supabaseClient';
-import { useEvent } from '../../../../EventContext'; 
-import { useTeam } from './../TeamContext'; 
+import { useEvent } from '../../../../EventContext';
+import { useTeam } from './../TeamContext';
 
 const UrgentAlerts = () => {
   const [urgentAlerts, setUrgentAlerts] = useState([]);
@@ -36,32 +37,32 @@ const UrgentAlerts = () => {
     async function fetchUrgentAlerts() {
       try {
         let query = supabase.from('vianney_alertes_specifiques').select("*").order('created_at', { ascending: false }).limit(5);
-        
+
         // Filter alerts based on the selected event ID
         if (selectedEventId) {
           query = query.eq('event_id', selectedEventId);
         }
-  
+
         const { data: alertsData, error: alertsError } = await query;
-  
+
         if (alertsError) {
           throw alertsError;
         }
-  
+
         // Fetch team names corresponding to teams_id
         const teamIds = alertsData.map(alert => alert.teams_id);
         const { data: teamsData, error: teamsError } = await supabase.from('vianney_teams').select('id, name_of_the_team').in('id', teamIds);
-  
+
         if (teamsError) {
           throw teamsError;
         }
-  
+
         // Create a map of team id to team name
         const teamNameMap = {};
         teamsData.forEach(team => {
           teamNameMap[team.id] = team.name_of_the_team;
         });
-  
+
         // Combine alert data with team names
         const enrichedAlertsData = alertsData.map(alert => ({
           ...alert,
@@ -70,21 +71,21 @@ const UrgentAlerts = () => {
 
         // Filter alerts based on the selected team
         const filteredAlerts = selectedTeam ? enrichedAlertsData.filter(alert => alert.team_name === selectedTeam) : enrichedAlertsData;
-  
+
         setUrgentAlerts(filteredAlerts);
         setIsLoading(false);
       } catch (error) {
         console.error("Error fetching urgent alerts:", error.message);
       }
     }
-  
+
     fetchUrgentAlerts();
   }, [selectedEventId, selectedTeam]);
-  
+
 
   const handleToggleReadStatus = async (id, currentStatus) => {
     try {
-      const {  error } = await supabase
+      const { error } = await supabase
         .from('vianney_alertes_specifiques')
         .update({ read_or_not: !currentStatus })
         .eq('id', id);
@@ -131,80 +132,80 @@ const UrgentAlerts = () => {
   };
 
   return (
-          <Box >
-        {isLoading ? (
-          <Spinner />
-        ) : (
-          <Stack spacing={4}>
-            {urgentAlerts.map((alert) => (
-              // Only render the alert if it's not read
-              !alert.read_or_not && (
-                <Alert
-                  key={alert.id}
-                  status="error"
-                  variant="subtle"
-                  flexDirection="column"
-                  alignItems="flex-start"
-                  justifyContent="center"
-                  textAlign="left"
-                  position="relative"
-                  width="100%"
-                  px={6}
-                  py={4}
-                  pr={14}
-                  rounded="md"
-                >
-                  <AlertIcon />
-                  <Stack spacing={2}>
-                    <Text fontWeight="bold" fontSize="lg">
-                      {alert.text_alert}
-                    </Text>
-                    <Button
-                      onClick={() =>
-                        handleToggleReadStatus(alert.id, alert.read_or_not)
-                      }
-                      size="sm"
-                      variant="outline"
-                      colorScheme="red"
-                    >
-                      Non lu (cliquez pour faire disparaître)
-                    </Button>
-                    <Text fontSize="sm" color="gray.500">
-                      Créé à : {new Date(alert.created_at).toLocaleString()}
-                    </Text>
-                  </Stack>
-                </Alert>
-              )
-            ))}
-          </Stack>
-        )}
-        <Modal isOpen={showResponseForm} onClose={() => setShowResponseForm(false)}>
-          <ModalOverlay />
-          <ModalContent>
-            <ModalHeader>Entrez la réponse</ModalHeader> 
-            <ModalCloseButton />
-            <ModalBody>
-              <FormControl>
-                <FormLabel>Réponse</FormLabel>
-                <Textarea
-                  value={responseText}
-                  onChange={(e) => setResponseText(e.target.value)}
-                  placeholder="Entrez votre réponse ici..."
-                  size="lg"
-                />
-                <FormHelperText>Entrez votre réponse à cette alerte urgente.</FormHelperText>
-              </FormControl>
-            </ModalBody>
-            <ModalFooter>
-              <Button colorScheme="blue" mr={3} onClick={handleSubmitResponse}>
-                Soumettre
-              </Button>
-              <Button onClick={() => setShowResponseForm(false)}>Annuler</Button>
-            </ModalFooter>
-          </ModalContent>
-        </Modal>
-      </Box>
-      );
+    <Box >
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <Stack spacing={4}>
+          {urgentAlerts.map((alert) => (
+            // Only render the alert if it's not read
+            !alert.read_or_not && (
+              <Alert
+                key={alert.id}
+                status="error"
+                variant="subtle"
+                flexDirection="column"
+                alignItems="flex-start"
+                justifyContent="center"
+                textAlign="left"
+                position="relative"
+                width="100%"
+                px={6}
+                py={4}
+                pr={14}
+                rounded="md"
+              >
+                <AlertIcon />
+                <Stack spacing={2}>
+                  <Text fontWeight="bold" fontSize="lg">
+                    {alert.text_alert}
+                  </Text>
+                  <Button
+                    onClick={() =>
+                      handleToggleReadStatus(alert.id, alert.read_or_not)
+                    }
+                    size="sm"
+                    variant="outline"
+                    colorScheme="red"
+                  >
+                    Non lu (cliquez pour faire disparaître)
+                  </Button>
+                  <Text fontSize="sm" color="gray.500">
+                    Créé le  {format(new Date(alert.created_at), "dd/MM/yyyy à HH:mm")}
+                  </Text>
+                </Stack>
+              </Alert>
+            )
+          ))}
+        </Stack>
+      )}
+      <Modal isOpen={showResponseForm} onClose={() => setShowResponseForm(false)}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Entrez la réponse</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <FormControl>
+              <FormLabel>Réponse</FormLabel>
+              <Textarea
+                value={responseText}
+                onChange={(e) => setResponseText(e.target.value)}
+                placeholder="Entrez votre réponse ici..."
+                size="lg"
+              />
+              <FormHelperText>Entrez votre réponse à cette alerte urgente.</FormHelperText>
+            </FormControl>
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={handleSubmitResponse}>
+              Soumettre
+            </Button>
+            <Button onClick={() => setShowResponseForm(false)}>Annuler</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </Box>
+  );
 };
 
 export default UrgentAlerts;
