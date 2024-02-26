4f0a93f11269eb7249b1cf2089c7884af3a4b9ca
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-02-26 17:47:49 +0000

    fdbgfgfgfgffggfgf

--- src/routes.js
+++ src/routes.js
@@ -15,27 +15,34 @@ import Parameters from "views/admin/Parameters";
 
 const routes = [
   {
-    name: "Ecran principal",
+    name: "Interface Equipe",
     layout: "/admin",
     path: "/default",
-    icon: <Icon as={FcSurvey} width='20px' height='20px' color='inherit' />,
-    component: MainDashboard,
+    icon: <Icon as={FcSelfie} width='20px' height='20px' color='inherit' />,
+    component: InterfaceEquipe,
   },
-  {
-    name: "Carte",
-    layout: "/admin",
-    path: "/map",
-    icon: <Icon as={FcMindMap} width='20px' height='20px' color='inherit' />,
-    component: NFTMarketplace,
-    secondary: true,
-  },
-  {
-    name: "Tableau de bord",
-    layout: "/admin",
-    path: "/data-tables",
-    icon: <Icon as={FcSerialTasks} width='20px' height='20px' color='inherit' />,
-    component: TableauDeBord,
-  },
+ // {
+ //   name: "Interface Equipe",
+ //   layout: "/admin",
+ //   path: "/interface-equipe",
+ //   icon: <Icon as={FcSelfie} width='20px' height='20px' color='inherit' />,
+  //  component: InterfaceEquipe,
+//  },
+ // {
+ //   name: "Carte",
+ //   layout: "/admin",
+ //   path: "/map",
+ //   icon: <Icon as={FcMindMap} width='20px' height='20px' color='inherit' />,
+ //   component: NFTMarketplace,
+ //   secondary: true,
+ // },
+//  {
+//    name: "Tableau de bord",
+//    layout: "/admin",
+//    path: "/data-tables",
+//    icon: <Icon as={FcSerialTasks} width='20px' height='20px' color='inherit' />,
+ //   component: TableauDeBord,
+ // },
   //
  // {
  //   name: "Profil des utilisateurs",
@@ -51,34 +58,28 @@ const routes = [
   //  icon: <Icon as={FcLock} width='20px' height='20px' color='inherit' />,
   //  component: SignInCentered,
  // },
-  {
-    name: "Paramètres",
-    layout: "/admin",
-    path: "/parameters",
-    icon: <Icon as={FcSettings} width='20px' height='20px' color='inherit' />,
-    component: Parameters,
-  },
-  {
-    name: "Interface Equipe",
-    layout: "/admin",
-    path: "/interface-equipe",
-    icon: <Icon as={FcSelfie} width='20px' height='20px' color='inherit' />,
-    component: InterfaceEquipe,
-  },
-  {
-    name: "Tableau Excel",
-    layout: "/admin",
-    path: "/tableau-excel",
-    icon: <Icon as={FcGrid} width='20px' height='20px' color='inherit' />,
-    component: TableauExcel,
-  },
-  {
-    name: "Alerte à une équipe", // Name of the new route
-    layout: "/admin",
-    path: "/alerte-equipe", // Path for the new route
-    icon: <Icon as={FcHighPriority} width='20px' height='20px' color='inherit' />, // You can use an appropriate icon component here
-    component: AlerteEquipe, // Component for the new route
-  },
+ // {
+ //   name: "Paramètres",
+ //   layout: "/admin",
+ //   path: "/parameters",
+ //   icon: <Icon as={FcSettings} width='20px' height='20px' color='inherit' />,
+ //   component: Parameters,
+//  },
+
+ // {
+ //   name: "Tableau Excel",
+ //   layout: "/admin",
+ //   path: "/tableau-excel",
+ //   icon: <Icon as={FcGrid} width='20px' height='20px' color='inherit' />,
+ //   component: TableauExcel,
+ // },
+ // {
+ //   name: "Alerte à une équipe", // Name of the new route
+ //   layout: "/admin",
+  //  path: "/alerte-equipe", // Path for the new route
+ //   icon: <Icon as={FcHighPriority} width='20px' height='20px' color='inherit' />, // You can use an appropriate icon component here
+  //  component: AlerteEquipe, // Component for the new route
+  //},
 ];
 
 export default routes;
