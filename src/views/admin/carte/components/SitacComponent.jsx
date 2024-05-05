63fd8cf4fb7fd51a4de111e049d9a49cd98d1d7d
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-05-05 20:16:43 +0000

    ffffffffffffff

--- src/views/admin/carte/components/SitacComponent.jsx
+++ src/views/admin/carte/components/SitacComponent.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 import { Box, Input, Image } from '@chakra-ui/react';
+import sitacImage from './../../../../assets/img/sitac.png'; // Assurez-vous que le chemin est correct
 
 function SitacComponent() {
   return (
     <Box position="relative" h="768px" w="1020px">
-      <Image src="/path_to_your_image/sitac.png" alt="Sitac Background" fit="cover" w="100%" h="100%" />
-      <Input position="absolute" top="10%" left="5%" w="18%" h="10%" placeholder="Situation" />
-      <Input position="absolute" top="10%" left="25%" w="18%" h="10%" placeholder="Anticipation" />
-      <Input position="absolute" top="10%" left="45%" w="18%" h="10%" placeholder="Objectif" />
-      <Input position="absolute" top="10%" left="65%" w="18%" h="10%" placeholder="Idée de manoeuvre" />
-      <Input position="absolute" top="10%" left="85%" w="10%" h="10%" placeholder="Exécution" />
-      <Input position="absolute" top="25%" left="85%" w="10%" h="15%" placeholder="Moyen Logistique" />
-      <Input position="absolute" top="55%" left="85%" w="10%" h="40%" placeholder="Commandement" />
+      <Image src={sitacImage} alt="Sitac Background" fit="cover" w="100%" h="100%" />
+      <Input position="absolute" top="70%" left="2%" w="15%" h="20%" placeholder="Situation" />
+      <Input position="absolute" top="70%" left="22%" w="15%" h="20%" placeholder="Anticipation" />
+      <Input position="absolute" top="70%" left="40%" w="15%" h="20%" placeholder="Objectif" />
+      <Input position="absolute" top="70%" left="60%" w="15%" h="20%" placeholder="Idée de manoeuvre" />
+      <Input position="absolute" top="70%" left="81%" w="15%" h="20%" placeholder="Exécution" />
+      <Input position="absolute" top="15%" left="76%" w="20%" h="20%" placeholder="Moyen Logistique" />
+      <Input position="absolute" top="42%" left="76%" w="20%" h="20%" placeholder="Commandement" />
     </Box>
   );
 }
