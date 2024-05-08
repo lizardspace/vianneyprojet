af1a45a7c5c0a9b7dbb808dc769376d54aa37f39
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-05-08 10:12:00 +0000

    ddddddddddddddddde

--- src/views/admin/carte/components/JSPaintComponent.jsx
+++ src/views/admin/carte/components/JSPaintComponent.jsx
@@ -4,7 +4,7 @@ const JSPaintComponent = () => {
     return (
         <div style={{ width: '100%', height: '100vh', border: 'none' }}>
             <iframe
-                src="https://663b3406291e6f0090c43d00--genuine-concha-944595.netlify.app/"
+                src="https://jspaint.app/#local:c4ed9982448a8"
                 style={{ width: '100%', height: '100%', border: 'none' }}
                 title="External Application"
             />
