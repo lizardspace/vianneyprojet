3292aacf4d7db4f3643cdad164cdf7b122548da1
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-05-08 19:29:19 +0000

    rrrrrrrrrrrrz

--- src/views/admin/carte/components/YandexImage.jsx
+++ src/views/admin/carte/components/YandexImage.jsx
@@ -4,7 +4,7 @@ const YandexImage = () => {
     return (
         <div style={{ width: '100%', height: '100vh', border: 'none' }}>
             <iframe
-                src="https://images.google.com/"
+                src="https://www.searchencrypt.com/home"
                 style={{ width: '100%', height: '100%', border: 'none' }}
                 title="External Application"
             />
