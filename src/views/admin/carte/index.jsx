6a47cadbc0ceedbdbd25d31a5bb0a9bfa3da531f
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-05-30 20:38:30 +0000

    hhhhhhhhv

--- src/views/admin/carte/index.jsx
+++ src/views/admin/carte/index.jsx
@@ -21,7 +21,6 @@ import LiveStreamsPage from './components/surveillance/LiveStreamsPage';
 import EditableRectangle from './components/SitacComponent';
 import JSPaintComponent from './components/JSPaintComponent';
 import YandexImage from './components/YandexImage';
-import TeamCharlieInfo from '../default/components/TeamCharlieInfo';
 
 export default function Marketplace() {
   // eslint-disable-next-line no-unused-vars
@@ -169,7 +168,6 @@ export default function Marketplace() {
                 </TabPanel>
                 <TabPanel>
                   {/* Content for "Effectif" sub-tab */}
-                  <TeamCharlieInfo/>
                   <EquipiersTableSimplify />
                 </TabPanel>
                 <TabPanel>
