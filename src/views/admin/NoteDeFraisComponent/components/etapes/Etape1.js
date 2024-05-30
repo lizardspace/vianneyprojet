826cc6c3e66516f4fe8645359dee2b56e256e824
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-05-30 20:58:15 +0000

    vhjfgjh

--- src/views/admin/NoteDeFraisComponent/components/etapes/Etape1.js
+++ src/views/admin/NoteDeFraisComponent/components/etapes/Etape1.js
@@ -26,36 +26,65 @@ function Etape1() {
       <Grid templateColumns="repeat(2, 1fr)" gap="6">
         <GridItem>
           <FormControl id="lastName">
-            <FormLabel>Nom</FormLabel>
-            <Input placeholder="Ex. Richard" />
+            <FormLabel fontWeight="bold" fontSize="sm">Nom</FormLabel>
+            <Input
+              placeholder="Ex. Richard"
+              borderColor="gray.300"
+              _placeholder={{ color: 'gray.500' }}
+              borderRadius="md"
+              padding="10px"
+            />
           </FormControl>
         </GridItem>
 
         <GridItem>
           <FormControl id="firstName">
-            <FormLabel>Prénom</FormLabel>
-            <Input placeholder="Ex. Louis" />
+            <FormLabel fontWeight="bold" fontSize="sm">Prénom</FormLabel>
+            <Input
+              placeholder="Ex. Louis"
+              borderColor="gray.300"
+              _placeholder={{ color: 'gray.500' }}
+              borderRadius="md"
+              padding="10px"
+            />
           </FormControl>
         </GridItem>
 
         <GridItem>
           <FormControl id="phoneNumber">
-            <FormLabel>Numéro de téléphone</FormLabel>
-            <Input placeholder="Ex. 0769094854" />
+            <FormLabel fontWeight="bold" fontSize="sm">Numéro de téléphone</FormLabel>
+            <Input
+              placeholder="Ex. 0769094854"
+              borderColor="gray.300"
+              _placeholder={{ color: 'gray.500' }}
+              borderRadius="md"
+              padding="10px"
+            />
           </FormControl>
         </GridItem>
 
         <GridItem>
           <FormControl id="email">
-            <FormLabel>Adresse mail</FormLabel>
-            <Input placeholder="Ex. louis.richard@ndc.com" />
+            <FormLabel fontWeight="bold" fontSize="sm">Adresse mail</FormLabel>
+            <Input
+              placeholder="Ex. louis.richard@ndc.com"
+              borderColor="gray.300"
+              _placeholder={{ color: 'gray.500' }}
+              borderRadius="md"
+              padding="10px"
+            />
           </FormControl>
         </GridItem>
 
         <GridItem>
           <FormControl id="pole">
-            <FormLabel>Choisir un pôle</FormLabel>
-            <Select placeholder="Choisir un pôle">
+            <FormLabel fontWeight="bold" fontSize="sm">Choisir un pôle</FormLabel>
+            <Select
+              placeholder="Choisir un pôle"
+              borderColor="gray.300"
+              borderRadius="md"
+              padding="10px"
+            >
               {/* Options for poles */}
             </Select>
           </FormControl>
@@ -63,8 +92,13 @@ function Etape1() {
 
         <GridItem>
           <FormControl id="polePrior">
-            <FormLabel>Choisir un pôle d'abord</FormLabel>
-            <Select placeholder="Choisir un pôle d'abord">
+            <FormLabel fontWeight="bold" fontSize="sm">Choisir un pôle d'abord</FormLabel>
+            <Select
+              placeholder="Choisir un pôle d'abord"
+              borderColor="gray.300"
+              borderRadius="md"
+              padding="10px"
+            >
               {/* Options for poles */}
             </Select>
           </FormControl>
@@ -72,24 +106,41 @@ function Etape1() {
 
         <GridItem colSpan={2}>
           <FormControl id="address">
-            <FormLabel>Adresse postale</FormLabel>
-            <Input placeholder="Ex. 3 avenue du général Mangin, 78000 Versailles" />
+            <FormLabel fontWeight="bold" fontSize="sm">Adresse postale</FormLabel>
+            <Input
+              placeholder="Ex. 3 avenue du général Mangin, 78000 Versailles"
+              borderColor="gray.300"
+              _placeholder={{ color: 'gray.500' }}
+              borderRadius="md"
+              padding="10px"
+            />
           </FormControl>
         </GridItem>
 
         <GridItem colSpan={2}>
           <FormControl id="rib">
-            <FormLabel>RIB</FormLabel>
-            <Textarea placeholder="Cliquez ici pour ajouter une photo ou un PDF" />
+            <FormLabel fontWeight="bold" fontSize="sm">RIB</FormLabel>
+            <Textarea
+              placeholder="Cliquez ici pour ajouter une photo ou un PDF"
+              borderColor="gray.300"
+              _placeholder={{ color: 'gray.500' }}
+              borderRadius="md"
+              padding="10px"
+            />
           </FormControl>
         </GridItem>
 
         <GridItem colSpan={2}>
           <FormControl id="donation">
-            <FormLabel>
+            <FormLabel fontWeight="bold" fontSize="sm">
               Souhaitez-vous abandonner vos frais au profit de Notre-dame de Chrétienté ?
             </FormLabel>
-            <Select placeholder="Choisir une option">
+            <Select
+              placeholder="Choisir une option"
+              borderColor="gray.300"
+              borderRadius="md"
+              padding="10px"
+            >
               <option value="yes">Oui</option>
               <option value="no">Non</option>
             </Select>
