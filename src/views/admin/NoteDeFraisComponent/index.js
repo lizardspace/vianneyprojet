f24b09074482c9b0533b0a2b12a71562658cf6b0
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-06-01 21:05:37 +0000

    dfsfgsdfgsdgdsgsdfds

--- src/views/admin/NoteDeFraisComponent/index.js
+++ src/views/admin/NoteDeFraisComponent/index.js
@@ -2,11 +2,15 @@
 import React from 'react';
 import { Box} from '@chakra-ui/react';
 import ExpenseForm from './components/ExpenseForm';
+import ExpenseList from './components/ExpenseList';
+import ExpenseReportContainer from './components/etapes/ExpenseReportContainer';
 
 const NoteDeFraisComponent = () => {
   return (
     <Box pt={{ base: "180px", md: "80px", xl: "80px" }}>
       <ExpenseForm/>
+      <ExpenseList/>
+      <ExpenseReportContainer/>
     </Box>
   );
 };
