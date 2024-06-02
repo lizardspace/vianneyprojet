8fd231f6ec24756f858474a113f16dfc69e36ed6
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-06-02 16:18:23 +0000

    dddddddddddds

--- src/views/admin/carte/components/FluxRssInput.jsx
+++ src/views/admin/carte/components/FluxRssInput.jsx
@@ -3,100 +3,102 @@ import { useEvent } from './../../../../EventContext'; // Assurez-vous que le ch
 import { supabase } from './../../../../supabaseClient';
 import { v4 as uuidv4 } from 'uuid';
 import {
-  Box,
-  Button,
-  FormControl,
-  FormLabel,
-  Input,
-  Text,
-  useToast,
+    Box,
+    Button,
+    FormControl,
+    FormLabel,
+    Input,
+    Text,
+    useToast,
 } from '@chakra-ui/react';
 
 const FluxRssInput = () => {
-  const { setEventId, selectedEventId } = useEvent();
-  const [eventList, setEventList] = useState([]);
-  const [rssUrl, setRssUrl] = useState('');
-  const toast = useToast();
+    // eslint-disable-next-line no-unused-vars
+    const { setEventId, selectedEventId } = useEvent();
+    // eslint-disable-next-line no-unused-vars
+    const [eventList, setEventList] = useState([]);
+    const [rssUrl, setRssUrl] = useState('');
+    const toast = useToast();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const { data, error } = await supabase.from('vianney_event').select('*');
-        if (error) {
-          throw error;
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const { data, error } = await supabase.from('vianney_event').select('*');
+                if (error) {
+                    throw error;
+                }
+                setEventList(data);
+            } catch (error) {
+                console.error('Error fetching data:', error.message);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        if (!selectedEventId || !rssUrl) {
+            toast({
+                title: 'Erreur',
+                description: 'Veuillez sélectionner un événement et saisir une URL RSS',
+                status: 'error',
+                duration: 5000,
+                isClosable: true,
+            });
+            return;
         }
-        setEventList(data);
-      } catch (error) {
-        console.error('Error fetching data:', error.message);
-      }
+
+        try {
+            const { error } = await supabase
+                .from('vianney_flux_rss')
+                .insert([{ id: uuidv4(), event_id: selectedEventId, url_du_flux_rss: rssUrl }]);
+
+            if (error) {
+                throw error;
+            }
+
+            toast({
+                title: 'Succès',
+                description: 'Flux RSS ajouté avec succès!',
+                status: 'success',
+                duration: 5000,
+                isClosable: true,
+            });
+            setRssUrl('');
+        } catch (error) {
+            console.error('Error inserting data:', error.message);
+        }
     };
 
-    fetchData();
-  }, []);
+    return (
+        <Box maxW="md" mx="auto" mt={8} p={4} borderWidth={1} borderRadius="md" boxShadow="lg">
+            <form onSubmit={handleSubmit}>
+                {selectedEventId ? (
+                    <FormControl mb={4}>
+                        <FormLabel>Événement sélectionné :</FormLabel>
+                        <Input type="text" value={selectedEventId} readOnly />
+                    </FormControl>
+                ) : (
+                    <Text mb={4}>Pas d'événement sélectionné</Text>
+                )}
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    if (!selectedEventId || !rssUrl) {
-      toast({
-        title: 'Erreur',
-        description: 'Veuillez sélectionner un événement et saisir une URL RSS',
-        status: 'error',
-        duration: 5000,
-        isClosable: true,
-      });
-      return;
-    }
+                <FormControl mb={4}>
+                    <FormLabel htmlFor="rssUrl">URL du Flux RSS :</FormLabel>
+                    <Input
+                        id="rssUrl"
+                        type="url"
+                        value={rssUrl}
+                        onChange={(e) => setRssUrl(e.target.value)}
+                    />
+                </FormControl>
 
-    try {
-      const { error } = await supabase
-        .from('vianney_flux_rss')
-        .insert([{ id: uuidv4(), event_id: selectedEventId, url_du_flux_rss: rssUrl }]);
-
-      if (error) {
-        throw error;
-      }
-
-      toast({
-        title: 'Succès',
-        description: 'Flux RSS ajouté avec succès!',
-        status: 'success',
-        duration: 5000,
-        isClosable: true,
-      });
-      setRssUrl('');
-    } catch (error) {
-      console.error('Error inserting data:', error.message);
-    }
-  };
-
-  return (
-    <Box maxW="md" mx="auto" mt={8} p={4} borderWidth={1} borderRadius="md" boxShadow="lg">
-      <form onSubmit={handleSubmit}>
-        {selectedEventId ? (
-          <FormControl mb={4}>
-            <FormLabel>Événement sélectionné :</FormLabel>
-            <Input type="text" value={selectedEventId} readOnly />
-          </FormControl>
-        ) : (
-          <Text mb={4}>Pas d'événement sélectionné</Text>
-        )}
-
-        <FormControl mb={4}>
-          <FormLabel htmlFor="rssUrl">URL du Flux RSS :</FormLabel>
-          <Input
-            id="rssUrl"
-            type="url"
-            value={rssUrl}
-            onChange={(e) => setRssUrl(e.target.value)}
-          />
-        </FormControl>
-
-        <Button type="submit" colorScheme="blue" width="full">
-          Ajouter Flux RSS
-        </Button>
-      </form>
-    </Box>
-  );
+                <Button type="submit" colorScheme="blue" width="full">
+                    Ajouter Flux RSS
+                </Button>
+            </form>
+        </Box>
+    );
 };
 
 export default FluxRssInput;
