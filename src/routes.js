727d63272b1f9153a44502d106d76338beda311d
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-06-03 16:51:18 +0000

    ssssssssssssssd

--- src/routes.js
+++ src/routes.js
@@ -1,21 +1,20 @@
 import React from "react";
 import { Icon } from "@chakra-ui/react";
 import { FcMindMap, FcSerialTasks, FcSurvey, FcSettings, FcSelfie, FcGrid, FcHighPriority, FcDiploma2 } from "react-icons/fc";
-import { MdQrCodeScanner } from "react-icons/md"; // Importe MdQrCodeScanner pour l'icône du nouveau route
+import { MdQrCodeScanner } from "react-icons/md";
+import { LuCross } from "react-icons/lu"; // Import the new icon
 
 import MainDashboard from "views/admin/default";
 import NFTMarketplace from "views/admin/carte";
-//import Profile from "views/admin/profile";
 import TableauDeBord from "views/admin/TableauDeBord";
 import InterfaceEquipe from "views/admin/InterfaceEquipe";
 import TableauExcel from "views/admin/TableauExcel";
 import AlerteEquipe from "views/admin/AlerteEquipe"; 
 import MaterialComponent from "views/admin/MaterialComponent";
 import ZoomedMapComponent from "views/admin/ZoomedMapComponent";
-import NoteDeFraisComponent from "views/admin/NoteDeFraisComponent"; // Assurez-vous d'avoir ce composant
+import NoteDeFraisComponent from "views/admin/NoteDeFraisComponent";
+import AlerteJeSuisEnDanger from "views/admin/alertejesuisendanger"; // Import the new component
 
-// Auth Imports
-//import SignInCentered from "views/auth/signIn";
 import Parameters from "views/admin/Parameters";
 
 const routes = [
@@ -42,10 +41,10 @@ const routes = [
     component: TableauDeBord,
   },
   {
-    name: "Carte zoomée", // Name of the new route
+    name: "Carte zoomée",
     layout: "/admin",
-    path: "/zoomed-map", // Path for the new route
-    component: ZoomedMapComponent, // Component for the new route
+    path: "/zoomed-map",
+    component: ZoomedMapComponent,
   },
   //
  // {
@@ -84,25 +83,32 @@ const routes = [
     component: TableauExcel,
   },
   {
-    name: "Alerte à une équipe", // Name of the new route
+    name: "Alerte à une équipe",
     layout: "/admin",
-    path: "/alerte-equipe", // Path for the new route
-    icon: <Icon as={FcHighPriority} width='20px' height='20px' color='inherit' />, // You can use an appropriate icon component here
-    component: AlerteEquipe, // Component for the new route
+    path: "/alerte-equipe",
+    icon: <Icon as={FcHighPriority} width='20px' height='20px' color='inherit' />,
+    component: AlerteEquipe,
   },
   {
-    name: "Matériel", // Name of the new route
+    name: "Matériel",
     layout: "/admin",
-    path: "/material", // Path for the new route
-    icon: <Icon as={MdQrCodeScanner} width='20px' height='20px' color='inherit' />, // Icon for the new route
-    component: MaterialComponent, // Component for the new route
+    path: "/material",
+    icon: <Icon as={MdQrCodeScanner} width='20px' height='20px' color='inherit' />,
+    component: MaterialComponent,
   },
   {
-    name: "Note de frais", // Name of the new route
+    name: "Note de frais",
     layout: "/admin",
-    path: "/note-de-frais", // Path for the new route
-    icon: <Icon as={FcDiploma2} width='20px' height='20px' color='inherit' />, // Icon for the new route
-    component: NoteDeFraisComponent, // Component for the new route
+    path: "/note-de-frais",
+    icon: <Icon as={FcDiploma2} width='20px' height='20px' color='inherit' />,
+    component: NoteDeFraisComponent,
+  },
+  {
+    name: "Secours",
+    layout: "/admin",
+    path: "/alertejesuisendanger",
+    icon: <Icon as={LuCross} width='20px' height='20px' color='inherit' />,
+    component: AlerteJeSuisEnDanger,
   },
 ];
 
