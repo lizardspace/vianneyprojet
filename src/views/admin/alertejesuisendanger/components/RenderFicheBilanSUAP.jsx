3522e40eaaf0c51744ebf7a2682960e1c7b7a916
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-06-15 19:55:09 +0000

    fdgfgdgdgfg

--- src/views/admin/alertejesuisendanger/components/RenderFicheBilanSUAP.jsx
+++ src/views/admin/alertejesuisendanger/components/RenderFicheBilanSUAP.jsx
@@ -8,14 +8,27 @@ const RenderFicheBilanSUAP = ({ data }) => {
 
   const handleDownloadPDF = async () => {
     const input = ficheRef.current;
-    const canvas = await html2canvas(input);
+    const canvas = await html2canvas(input, { scale: 2 });
     const imgData = canvas.toDataURL('image/png');
     const pdf = new jsPDF('p', 'mm', 'a4');
-    const imgProps = pdf.getImageProperties(imgData);
     const pdfWidth = pdf.internal.pageSize.getWidth();
-    const pdfHeight = (imgProps.height * pdfWidth) / imgProps.width;
+    const pdfHeight = pdf.internal.pageSize.getHeight();
+    const imgProps = pdf.getImageProperties(imgData);
+    const imgHeight = (imgProps.height * pdfWidth) / imgProps.width;
 
-    pdf.addImage(imgData, 'PNG', 0, 0, pdfWidth, pdfHeight);
+    let heightLeft = imgHeight;
+    let position = 0;
+
+    pdf.addImage(imgData, 'PNG', 0, position, pdfWidth, imgHeight);
+    heightLeft -= pdfHeight;
+
+    while (heightLeft >= 0) {
+      position = heightLeft - imgHeight;
+      pdf.addPage();
+      pdf.addImage(imgData, 'PNG', 0, position, pdfWidth, imgHeight);
+      heightLeft -= pdfHeight;
+    }
+
     pdf.save(`fiche_bilan_${data.nom}_${data.prenom}_N°INTER_${data.inter_number}.pdf`);
   };
 
