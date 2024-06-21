afe8c37832d6e411649bcb4eca54e0ecae3b3790
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-06-21 12:53:26 +0000

    sdfgsfdsfdsfd

--- src/views/admin/ChatComponent/index.jsx
+++ src/views/admin/ChatComponent/index.jsx
@@ -1,11 +1,12 @@
 // views/admin/NoteDeFraisComponent.jsx
 import React from 'react';
 import { Box} from '@chakra-ui/react';
+import MessagerieWhatsappChat from './component/MessagerieWhatsappChat';
 
 const NoteDeFraisComponent = () => {
   return (
     <Box pt={{ base: "180px", md: "80px", xl: "80px" }}>
-
+        <MessagerieWhatsappChat/>
     </Box>
   );
 };
