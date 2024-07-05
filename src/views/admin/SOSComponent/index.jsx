38d6222ce839f3c5d1e971bb995b211abaad2252
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-07-05 13:18:16 +0000

    rfgsdfsdcfs

--- src/views/admin/SOSComponent/index.jsx
+++ src/views/admin/SOSComponent/index.jsx
@@ -1,14 +1,32 @@
-import React from "react";
-import { Box } from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Box, Button, Modal, ModalOverlay, ModalContent, ModalHeader, ModalFooter, ModalBody, ModalCloseButton, useDisclosure } from "@chakra-ui/react";
 import AccidentDetected from "./components/AccidentDetected";
 import SOSAlertsView from "./components/SOSAlertsView";
 
-
 export default function Settings() {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
     <Box pt={{ base: "130px", md: "80px", xl: "80px" }}>
-        <AccidentDetected/>      
-        <SOSAlertsView/>
+      <SOSAlertsView />
+      <Button onClick={onOpen} mt={4}>
+        Déclencher un SOS
+      </Button>
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader></ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <AccidentDetected />
+          </ModalBody>
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3} onClick={onClose}>
+              Fermer
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
