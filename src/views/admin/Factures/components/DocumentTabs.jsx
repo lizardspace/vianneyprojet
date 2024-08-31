ec7de95b0a5a0243b4319dd93bfde44de4d4838a
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-08-31 15:45:07 +0000

    qqqqqqqqqqqqqqqqsss

--- src/views/admin/Factures/components/DocumentTabs.jsx
+++ src/views/admin/Factures/components/DocumentTabs.jsx
@@ -4,52 +4,52 @@ import { FcMoneyTransfer, FcDiploma2 } from "react-icons/fc";
 import { useHistory, useLocation } from "react-router-dom";
 
 const DocumentTabs = () => {
-  const history = useHistory();
-  const location = useLocation();
+    const history = useHistory();
+    const location = useLocation();
 
-  // Determine which tab should be active based on the current path
-  const determineActiveIndex = useCallback(() => {
-    switch (location.pathname) {
-      case "/admin/factures":
-        return 0;
-      case "/admin/note-de-frais":
-        return 1;
-      default:
-        return 0;
-    }
-  }, [location.pathname]);
+    // Determine which tab should be active based on the current path
+    const determineActiveIndex = useCallback(() => {
+        switch (location.pathname) {
+            case "/admin/factures":
+                return 0;
+            case "/admin/note-de-frais":
+                return 1;
+            default:
+                return 0;
+        }
+    }, [location.pathname]);
 
-  const [tabIndex, setTabIndex] = React.useState(determineActiveIndex());
+    const [tabIndex, setTabIndex] = React.useState(determineActiveIndex());
 
-  // Update the tab index when the location changes
-  useEffect(() => {
-    setTabIndex(determineActiveIndex());
-  }, [location.pathname, determineActiveIndex]);
+    // Update the tab index when the location changes
+    useEffect(() => {
+        setTabIndex(determineActiveIndex());
+    }, [location.pathname, determineActiveIndex]);
 
-  const handleTabClick = (index, path) => {
-    setTabIndex(index);
-    history.push(path);
-  };
+    const handleTabClick = (index, path) => {
+        setTabIndex(index);
+        history.push(path);
+    };
 
-  return (
-    <Tabs index={tabIndex} variant="soft-rounded" colorScheme="blue">
-      <TabList>
-        <Tab onClick={() => handleTabClick(0, "/admin/factures")}>
-          <Icon as={FcMoneyTransfer} mr={2} />
-          Gestion des Factures
-        </Tab>
-        <Tab onClick={() => handleTabClick(1, "/admin/note-de-frais")}>
-          <Icon as={FcDiploma2} mr={2} />
-          Comptabilité
-        </Tab>
-      </TabList>
+    return (
+        <Tabs index={tabIndex} variant="soft-rounded" colorScheme="blue">
+            <TabList>
+                <Tab onClick={() => handleTabClick(1, "/admin/note-de-frais")}>
+                    <Icon as={FcDiploma2} mr={2} />
+                    Comptabilité
+                </Tab>
+                <Tab onClick={() => handleTabClick(0, "/admin/factures")}>
+                    <Icon as={FcMoneyTransfer} mr={2} />
+                    Gestion des Factures
+                </Tab>
+            </TabList>
 
-      <TabPanels>
-        <TabPanel />
-        <TabPanel />
-      </TabPanels>
-    </Tabs>
-  );
+            <TabPanels>
+                <TabPanel />
+                <TabPanel />
+            </TabPanels>
+        </Tabs>
+    );
 };
 
 export default DocumentTabs;
