0d15bb6387bee334f218b7c081eedf210317037d
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-09-07 13:10:33 +0000

    dddddddddddff

--- src/views/admin/AddDocumentComponent/index.jsx
+++ src/views/admin/AddDocumentComponent/index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import { Box } from "@chakra-ui/react";
 import DocumentationsComponent from "../default/DocumentionsComponent/DocumentationsComponent";
 import { useEvent } from '../../../EventContext'; // Import the useEvent hook
+import DocumentTabs from "../alertejesuisendanger/components/DocumentTabs";
 
 
 const AddDocumentComponent = () => {
     const { selectedEventId } = useEvent();
     return (
         <Box pt={{ base: '180px', md: '80px', xl: '80px' }}>
+                  <DocumentTabs />
             {selectedEventId && (
                 <DocumentationsComponent eventId={selectedEventId} />
             )}
