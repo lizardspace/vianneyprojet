8bfb76caec8d5a36a6c1f0d6ca5854c0fc51f037
lizardspace <154006119+lizardspace@users.noreply.github.com>
2024-09-09 19:09:17 +0000

    ssssssssssssssssssq

--- src/views/admin/carte/ToggleComponentGpsPointForm.jsx
+++ src/views/admin/carte/ToggleComponentGpsPointForm.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Box, IconButton, VStack } from '@chakra-ui/react';
+import { Box, IconButton, VStack, Tooltip } from '@chakra-ui/react';
 import { AddIcon, MinusIcon } from '@chakra-ui/icons';
 import GpsPointForm from './GpsPointForm';  // Importer votre GpsPointForm
 
@@ -12,20 +12,21 @@ const ToggleComponentGpsPointForm = () => {
 
   return (
     <VStack align="start" spacing={4}>
-      {/* Bouton avec icône pour montrer/masquer le GpsPointForm */}
-      <IconButton
-        aria-label={isVisible ? 'Masquer le formulaire GPS' : 'Montrer le formulaire GPS'}
-        icon={isVisible ? <MinusIcon /> : <AddIcon />}
-        onClick={toggleVisibility}
-        colorScheme="teal"
-      />
+      {/* Tooltip avec le message et le bouton avec icône pour montrer/masquer le GpsPointForm */}
+      <Tooltip label="Centrer la carte sur un point fixe" aria-label="Centrer la carte">
+        <IconButton
+          aria-label={isVisible ? 'Masquer le formulaire GPS' : 'Montrer le formulaire GPS'}
+          icon={isVisible ? <MinusIcon /> : <AddIcon />}
+          onClick={toggleVisibility}
+          colorScheme="teal"
+        />
+      </Tooltip>
 
       {/* Formulaire GPS qui sera affiché ou masqué */}
       {isVisible && (
         <Box
           w="100%"
           p={4}
-          bg="teal.100"
           borderRadius="md"
           shadow="md"
         >
